b2358adf3e6086b11e6f1b31a3c68131fcad1ed9
Damien RAYMOND <dam.ray49@gmail.com>
2017-06-20 15:04:08 +0200

    Add function isWeekEnd

--- app/ng-weekly-scheduler/js/services/time-services.js
+++ app/ng-weekly-scheduler/js/services/time-services.js
@@ -42,6 +42,9 @@ angular.module('weeklyScheduler')
       weekPreciseDiff: function (start, end) {
         return end.clone().diff(start.clone(), WEEK, true);
       },
+      isWeekEnd: function (date) {
+        return date.isoWeekday() === 6 || date.isoWeekday() === 7;
+      },
       dayDiff: function (start, end) {
         return end.clone().endOf(DAY).diff(start.clone().startOf(DAY), DAY) + 1;
       },
